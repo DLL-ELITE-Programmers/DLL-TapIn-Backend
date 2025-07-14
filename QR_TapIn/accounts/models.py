bfabc720c8e435cc3ec32b46847792835bfb5ecd
RyannKim327 <rksesgundo123@gmail.com>
2025-07-14 16:38:04 +0800

    07-14-25 16:37 [Added Accounts APP]

--- QR_TapIn/accounts/models.py
+++ QR_TapIn/accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
+
+class Department(models.Model):
+    department_id = models.CharField(max_length=10, default="BSIT")
+    department_name = models.CharField(max_length=100, default="Bachelor of Science in Information Technology")
+
+    def __str__(self):
+        return f"{self.department_id}"
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    middle_name = models.CharField(
+        max_length=25, null=True, blank=True, default="", editable=True
+    )
+    sex = models.IntegerField(
+        choices=[(0, "Female"), (1, "Male"), (2, "Others")], default=2
+    )
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="user", default="", null=True, blank=True)
