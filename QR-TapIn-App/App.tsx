02f695e8f6aaa8c286d41e15943c593b2f44b7cb
RyannKim327 <rksesgundo123@gmail.com>
2025-07-17 23:17:49 +0800

    07-17-25 23:17 - @RyannKim327 [LOGIN Done]

--- QR-TapIn-App/App.tsx
+++ QR-TapIn-App/App.tsx
@@ -6,24 +6,23 @@ import { get, get_unauth } from "./utils/access";
 import Page from "./src/pages";
 
 export default function App() {
-  const [data, setData] = useState<unknown[]>([]);
-  const [isStatusbarVisible, setStatusbarVisible] = useState(true)
+  // const [data, setData] = useState<unknown[]>([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await get_unauth("api/users");
+  //     setData(response);
+  //   })();
+  // }, []);
 
-  useEffect(() => {
-    (async () => {
-      const response = await get_unauth("api/users");
-      setData(response);
-    })();
-  }, []);
+  console.log(Math.round(sb.currentHeight));
 
-  useEffect(() => {
-    setStatusbarVisible(sb.currentHeight === null)
-  }, [sb.currentHeight])
   return (
     <View className="flex flex-col bg-[#fff] w-full h-full">
       <StatusBar style="auto" />
       {/* NOTE: mt-[30px] was the one who created the gap for the statusbar */}
-      <View className={`bg-slate-400 ${isStatusbarVisible ? "mt-[30px]" : ""} w-full h-full flex-1 items-center justify-center`}>
+      <View
+        className={`bg-slate-500 android:mt-[30px] ios:mt-[30px] w-full h-full flex-1 items-center justify-center`}
+      >
         {/* <Text>Open up App.tsx to start working on your app!</Text>
         <Text>Basta miss ko na sya.</Text>
         {data.map((user: unknown, index: number) => {
--- QR-TapIn-App/src/widgets/input.tsx
+++ QR-TapIn-App/src/widgets/input.tsx
@@ -2,17 +2,25 @@ import { Text, TextInput } from "react-native";
 import { View } from "react-native";
 
 interface input {
-    name: string
+  name: string;
+  password?: boolean;
+  hint?: string;
 }
 
-export default function Input(props: input){
-    const capitalized = (text: string) => {
-        return text[0].toUpperCase() + text.substring(1)
-    }
-    return (
-        <View className={`w-full`}>
-            <Text className="text-sm">{capitalized(props.name)}</Text>
-            <TextInput className="border-[2px] border-[#60affe] rounded-[5px]" />
-        </View>
-    )
+export default function Input(props: input) {
+  const capitalized = (text: string) => {
+    text = text.replace(/_/g, " ");
+    return text[0].toUpperCase() + text.substring(1);
+  };
+
+  return (
+    <View className={`w-full`}>
+      <Text className="text-sm">{capitalized(props.name)}</Text>
+      <TextInput
+        placeholder={props.hint}
+        secureTextEntry={props.password ?? false}
+        className="border-[2px] border-black/50 focus:border-[#60affe] rounded-[5px]"
+      />
+    </View>
+  );
 }
