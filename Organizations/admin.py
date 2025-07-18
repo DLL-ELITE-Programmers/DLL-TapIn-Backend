2d21c5ebd8dd7b3a73055650c1980ea5e5725496
RyannKim327 <rksesgundo123@gmail.com>
2025-07-18 18:16:46 +0800

    07-18-25 18:16 [Included organization]

--- Organizations/admin.py
+++ Organizations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Organizations.models import Department
+from Organizations.models import Department, Organization
 
 # Register your models here.
 admin.site.register(Department)
+admin.site.register(Organization)
