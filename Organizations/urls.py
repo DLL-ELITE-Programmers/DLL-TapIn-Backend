e5bb8375757903420c74340072c034b2617621b3
RyannKim327 <rksesgundo123@gmail.com>
2025-07-24 00:21:30 +0800

    07-24-25  0:21 - @RyannKim327

--- Organizations/urls.py
+++ Organizations/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from Organizations.viewsets.department import DepartmentViewset
+from Organizations.viewsets.organization import OrganizationViewset
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
 router.register(r"department", DepartmentViewset, basename="department")
+router.register(r"organization", OrganizationViewset, basename="organizations")
 
 urlpatterns = [path("", include(router.urls))]
