a662681c402cad36ba72ba10a30fbe6fc2388932
RyannKim327 <rksesgundo123@gmail.com>
2025-07-30 07:17:09 +0800

    07-30-25  7:17 - @RyannKim327 [server pilot added user is already participating issue]

--- Events/viewsets/participants.py
+++ Events/viewsets/participants.py
@@ -16,6 +16,14 @@ class ParticipantViewset(BaseAuthModelViewset):
         try:
             query = self.request.query_params
 
+            if query.get("event"):
+                self.queryset = Participant.objects.filter(
+                    event__exact=query.get("event")
+                )
+            if query.get("user"):
+                userID = User.objects.get(username=query.get("user").upper()).id
+                self.queryset = Participant.objects.filter(participant__exact=userID)
+
             data = self.serializer_class(self.queryset.all(), many=True)
             return Response(data.data)
 
@@ -27,22 +35,20 @@ class ParticipantViewset(BaseAuthModelViewset):
             data = request.data
             event = Event.objects.get(event_id=data["event"]).event_id
             participant = User.objects.get(username__iexact=data["participant"]).id
+
+            existing = Participant.objects.filter(
+                event=event, participant=participant
+            ).exists()
+
+            if existing:
+                return Response({"error": "The student is already registered"})
+
             serialize = self.serializer_class(
                 data={"event": event, "participant": participant}, partial=True
             )
             if serialize.is_valid():
                 serialize.save()
                 return Response({"message": "Student is now participating"})
-            return Response({"error": self.extract_error_handler(serialize.errors)})
-        except Exception as e:
-            return Response({"error": str(e)})
-
-        try:
-            data = request.data
-            serialize = self.serializer_class(data, partial=False)
-            if serialize.is_valid():
-                serialize.save()
-                return Response({"message": "Participant registed"})
             return Response({"error": self.extract_error_handler(serialize.errors)})
         except Exception as e:
             return Response({"error": str(e)})
